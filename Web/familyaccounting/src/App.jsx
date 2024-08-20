cfed73f9994b93e8901942829b618f95efee226a
alers16 <alerosan16@gmail.com>
2024-08-21 00:23:14 +0200

    Home done

--- Web/familyaccounting/src/App.jsx
+++ Web/familyaccounting/src/App.jsx
@@ -3,8 +3,9 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
 import Header from "./components/Header";
+import Form from "./pages/Formulario/Formulario";
 
 function App() {
   const wrap = (v) => (
@@ -19,6 +20,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={wrap(<Home />)} />
+          <Route path="/form" element={wrap(<Form />)} />
         </Routes>
       </BrowserRouter>
     </div>
